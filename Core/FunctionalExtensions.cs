d7e046bd543a174313bebf29e1dabdd24a9d4d3d
rubenvh <ruben.van.havermaet@gmail.com>
2026-03-16 08:55:21 +0100

    Add async callback overloads to Tap (sync+async, async+async)

--- Core/FunctionalExtensions.cs
+++ Core/FunctionalExtensions.cs
@@ -14,18 +14,33 @@ public static class FunctionalExtensions
         action(source);
         return source;
     }
-    
+
     /// <summary>
-    /// This function allows you to perform an action on an object without changing the object itself.
+    /// Async Tap: sync source, async action.
     /// </summary>
-    /// <param name="source"></param>
-    /// <param name="action"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    public static async Task<T> Tap<T>(this T source, Func<T, Task> action)
+    {
+        await action(source);
+        return source;
+    }
+
+    /// <summary>
+    /// Async Tap: async source, sync action.
+    /// </summary>
     public static async Task<T> Tap<T>(this Task<T> source, Action<T> action)
     {
         var result = await source;
         action(result);
+        return result;
+    }
+
+    /// <summary>
+    /// Async Tap: async source, async action.
+    /// </summary>
+    public static async Task<T> Tap<T>(this Task<T> source, Func<T, Task> action)
+    {
+        var result = await source;
+        await action(result);
         return result;
     }
 }
